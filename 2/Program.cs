109352e2bacaa8e389b69f46a7f5b548ed9b9b31
CecilieSM <ceciliek1989@outlook.com>
2025-02-25 13:31:40 +0100

    Add files via upload

---
+++ 2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fahrenheit (˚F) til Celsius (˚C)");
+            int fahrenheit = 32;
+            int celsius = 40;
+            double convert = celsius * (9f / 5f) + 32;
+            Console.WriteLine((9 / 5) + 32);
+            Console.WriteLine($"Celsius til Fahrenheit {convert}");
+
+
+            Console.WriteLine("Celsius (˚C) til Fahrenheit (˚F)");
+            int celsius2 = 20;
+            int fahrenheit2 = -32;
+            double convert2 = fahrenheit2 -32 * (5f / 9f);
+            Console.WriteLine(-32 + (5/9));
+            Console.WriteLine($"Fahrenheit til Celsius {convert2}");
+
+            Console.ReadKey();
+        }
+    }
+}
