b1f42ac6abb00fd5bb919b6f04c611b1ac5e39f2
Sabine Sosic <197887348+lillesakat@users.noreply.github.com>
2025-02-25 12:54:40 +0100

    Add files via upload
    opgave 4,5,6

---
+++ 4.5.6/opgave6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace opgave6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Brug logiske og sammenligningsoperatører til at evaluere udtryk og udskriv resultaterne. 
+
+
+            int a = 5;
+            int b = 10;
+
+            bool isEqual = (a == b); 
+            bool isNotEqual = (a != b); 
+            bool isGreater = (a > b); 
+            bool isLess = (a < b); 
+            bool isGreaterOrEqual = (a >= b); 
+            bool isLessOrEqual = (a <= b); 
+
+            Console.WriteLine("Is Equal: " + isEqual + ", Is Not Equal: " + isNotEqual + ", Is Greater: " + isGreater + ", Is Less: " + isLess + ", Is Greater or Equal: " + isGreaterOrEqual + ", Is Less or Equal: " + isLessOrEqual); 
+
+        }
+    }
+}
