f64461f33604206656eb96193923ee209cb0119b
CecilieSM <ceciliek1989@outlook.com>
2025-02-25 13:29:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BMI-beregner");
+            Console.WriteLine();
+            Console.WriteLine("Indtast vægt (kg)");
+            string weightInput = Console.ReadLine();
+            double weight;
+            
+
+            Console.WriteLine("Indtast højde (meter)");
+            string heightInput = Console.ReadLine();
+            double height;
+
+
+            if (double.TryParse(weightInput, out weight) && double.TryParse(heightInput, out height))
+            {
+                //Beregn BMI
+                double bmi = weight / Math.Pow(height, 2);
+                Console.WriteLine($"BMI er: + {bmi}");
+            }
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
